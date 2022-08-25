27bd52a34b90db22128f87feed6e579befe1584f
xFuture603 <daniel.xfuture@googlemail.com>
2022-08-25 12:20:01 +0200

    edited Header for T-Systems page
    Co-authored-by: artstand <artstand@users.noreply.github.com>
    Co-authored-by: Bieger, Sebastian <Sebastian.Bieger@t-systems.com>
    Co-authored-by: Christopher Grau <beechesII@users.noreply.github.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ export default function Header() {
   return (
     <div class="flex">
         <div class="flex-none">
-            <scale-app-header claim-lang="en" logo-title="App Name" logo-href="/" addonNavigation='[{"name":"Our Projects","id":"Carousell","href":"#carousell"},{"name":"Contact","id":"Contact","href":"#contact"}]'></scale-app-header>
+            <scale-app-header class="header" claim-lang="en" logo-title="T-Systems Logo" addon-navigation='[{&quot;name&quot;:&quot;Our Projects&quot;,&quot;id&quot;:&quot;Carousell&quot;,&quot;href&quot;:&quot;#carousell&quot;},{&quot;name&quot;:&quot;Contact&quot;,&quot;id&quot;:&quot;Contact&quot;,&quot;href&quot;:&quot;#contact&quot;}]'>
+            </scale-app-header>
         </div>
     </div>
   );
