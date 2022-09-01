cba3d59f52a928c8ea4cfc6f6a852c8d106acc4b
xFuture603 <daniel.xfuture@googlemail.com>
2022-09-01 11:09:05 +0200

    added Logo for Header and Logo as SVG
    Co-authored-by: artstand <artstand@users.noreply.github.com>
    Co-authored-by: Christoph <z-bsod@users.noreply.github.com>
    Co-authored-by: Christopher Grau <beechesII@users.noreply.github.com>
    Co-authored-by: Bieger, Sebastian <Sebastian.Bieger@t-systems.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
+
+import T_Systems_MMS_Logo from "../images/T_Systems_MMS_Logo.svg";
+
 export default function Header() {
   return (
     <div class="flex">
         <div class="flex-none">
             <scale-app-header class="header" claim-lang="en" logo-title="T-Systems Logo" addon-navigation='[{&quot;name&quot;:&quot;Our Projects&quot;,&quot;id&quot;:&quot;Carousell&quot;,&quot;href&quot;:&quot;#carousell&quot;},{&quot;name&quot;:&quot;Contact&quot;,&quot;id&quot;:&quot;Contact&quot;,&quot;href&quot;:&quot;#contact&quot;}]'>
+              <div slot="logo">
+                <img src={T_Systems_MMS_Logo} class="logo"/>
+              </div>
             </scale-app-header>
         </div>
     </div>
